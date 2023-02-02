b539b664010e1e2a1dd6348e75e94d8f63fa1592
junyeoplee <jyeop920@gmail.com>
2023-02-02 17:27:07 +0900

    생성

---
+++ react/tutorial/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const ClickHandler = () => {
+    
+  }
+  return (
+    <div className="App">
+      <h1 className='text'>Hello World</h1>
+      <button>click</button>
+    </div>
+  );
+}
+
+export default App;
