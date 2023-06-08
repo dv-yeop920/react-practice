1dc5a2b43516d78d636c95ec97b092219d765fea
junyeoplee <jyeop920@gmail.com>
2023-06-09 01:51:36 +0900

    input 수정 최종
    경고문이 뜬 후에도 input에 value가 남아 있고 지울때마다 상태가 변경 되서 계속 useEffect 실행이 되었기 때문에
    clean up 함수를 지우고 메세지가 뜨면 setinput을 초기화 해주는 코드 추가 후
    html 에 value 를 userinput과 연결 시켜 초기화 되면 html에도 적용 되도록 수정 끝

--- react/tutorial/src/components/ShoppingInput.jsx
+++ react/tutorial/src/components/ShoppingInput.jsx
@@ -5,13 +5,18 @@ import { useState } from 'react';
 const ShoppingInput = () => {
     const [userInputValue , setUserInputValue] = useState('');
     useEffect(() => {
-        /[/^$|\D+/]/.test(userInputValue) && alert('숫자만 입력바람');
+        //숫자나 빈 문자열이 아니라면
+        if(/[/^$|\D+/]/.test(userInputValue)) {
+            alert('숫자만 입력바람');
+            setUserInputValue('');
+        }
     },[userInputValue]);
     //useEffect 써서 숫자만 입력할수 있게 하고  그 이외의 문자 입력하면 alert 띄우기
     return (
         <>
         <input 
         onChange={(e) => {setUserInputValue(e.target.value)}}
+        value = {userInputValue}
         type="text"
         placeholder='입력...'/>
         </>
