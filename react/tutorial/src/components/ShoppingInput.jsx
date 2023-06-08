4982598edea9d24e627255020b648e3622f35a17
junyeoplee <jyeop920@gmail.com>
2023-06-09 01:25:45 +0900

    input useEffect 수정
    정규표현식으로 빈문자열과 숫자가 아닐경우 메세지가 뜨도록 함

--- react/tutorial/src/components/ShoppingInput.jsx
+++ react/tutorial/src/components/ShoppingInput.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const ShoppingInput = () => {
     const [userInputValue , setUserInputValue] = useState('');
     useEffect(() => {
-        !/[0-9]/.test(userInputValue) && alert('숫자만 입력바람');
+        /[/^$|\D+/]/.test(userInputValue) && alert('숫자만 입력바람');
     },[userInputValue]);
     //useEffect 써서 숫자만 입력할수 있게 하고  그 이외의 문자 입력하면 alert 띄우기
     return (
