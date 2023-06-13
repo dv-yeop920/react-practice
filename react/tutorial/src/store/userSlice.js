8a385c6339ae58b5fc98ff1977687aa38bbe36a1
junyeoplee <jyeop920@gmail.com>
2023-06-14 08:32:55 +0900

    동일 상품이 있으면 추가 안되고 count만 증가 하도록 수정
    find 로 같은 상품이 있다면 count 추가
    아니면 push 하도록 함.
    여기서 state의 index는 findindex로 원래의 값과 새로 추가된 값이 동일한 아이디일 경우 그 index를 반환한 것

--- react/tutorial/src/store/userSlice.js
+++ react/tutorial/src/store/userSlice.js
@@ -9,24 +9,30 @@ const user = createSlice({
     reducers: {
         addCount(state , action){
             const selectId = state.find(item => {
-                return action.payload === item.id
-            })
+                return action.payload === item.id;
+            });
             selectId.count++;
         },
         subtractCount(state , action) {
             const selectId = state.find(item => {
-                return action.payload === item.id
-            })
+                return action.payload === item.id;
+            });
             selectId.count--;
             selectId.count < 1 && selectId.count++;
         },
         addUserList(state , action) {
-            state.push(action.payload);
+            const newList = action.payload;
+            const stateIndex = state.findIndex(list => {
+                return list.id === newList.id;
+            });
+            state.find(list => list.id === newList.id) 
+            ?  state[stateIndex].count++ 
+            : state.push(newList);
         },
         removeUserList(state , action) {
             const selectId = state.findIndex(item => {
-                return action.payload === item.id
-            })
+                return action.payload === item.id;
+            });
             state.splice(selectId , 1);
         }
     }
