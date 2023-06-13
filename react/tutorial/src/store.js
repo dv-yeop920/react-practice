ba597f87aa125b99717dbd230a0f40e1e41e26d7
junyeoplee <jyeop920@gmail.com>
2023-06-13 23:50:21 +0900

    store 폴더 만들어서 코드 분리
    한곳에 여러 스테이트를 만들면 가독성이 매우 떨어지기 때문에 각 역할 별로 state를 나눠서 store 폴더에 나눠 놓고 import하는 형식으로 사용하면 편함

--- react/tutorial/src/store.js
+++ react/tutorial/src/store.js
@@ -1,21 +1,5 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
-
-
-const user = createSlice({
-    name:'user',
-    initialState: 
-    [
-        {id : 0, name : 'White and Black', count : 2},
-        {id : 1, name : 'Grey Yordan', count : 1},
-        {id : 2, name : 'hey', count : 5},
-        {id : 3, name : 'hello world', count : 3}
-    ],
-    reducers: {
-        changeCount(state , action){
-            state[action.payload].count += 1;
-        }
-    }
-});
+import user from './store/userSlice.js';
 
 
 export const { changeCount } = user.actions;
---
+++ react/tutorial/src/store/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const user = createSlice({
+    name:'user',
+    initialState: 
+    [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 1, name : 'Grey Yordan', count : 1},
+        {id : 2, name : 'hey', count : 5},
+        {id : 3, name : 'hello world', count : 3}
+    ],
+    reducers: {
+        changeCount(state , action){
+            state[action.payload].count += 1;
+        }
+    }
+});
+
+export default user
